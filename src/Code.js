ca86293ee1ebe9b0b6ed81a02e87ff33ff23d06c
ratiotile <eujain@gmail.com>
2018-01-11 13:33:08 -0500

    initial implementation of air unit calc running, but has bugs

--- src/Code.js
+++ src/Code.js
@@ -20,7 +20,10 @@ function createUnitSheet(data){
 
 /** Takes a DataRange containing the full base unit statistics
 */
-function calculateAirUnits(baseUnitDataRange){
+function calculateAirUnits(){
+  // For now, assume there is a named range for base unit stats
+  var baseUnitDataRange = SpreadsheetApp.getActiveSpreadsheet()
+    .getRangeByName('BaseUnitStats')
   // note: air defense is bugged and doesn't work!
   var relevantColumns = ["soft_attack", "hard_attack", "air_attack",
     "strategic_attack", "range", "air_detection",
@@ -28,14 +31,18 @@ function calculateAirUnits(baseUnitDataRange){
     "build_cost_ic", "build_time", "supply_consumption", "fuel_consumption"
   ]
   var columnIndices = [] // corresponds to relevantColumns
-  var relevantUnits = ["interceptor", "multi_role", "cas", "cag",
+  var relevantUnits = ["interceptor"]
+
+/*
+  ["interceptor", "multi_role", "cas", "cag",
     "tactical_bomber", "naval_bomber", "strategic_bomber", "transport_plane",
     "rocket_interceptor", "flying_bomb", "flying_rocket"
-  ]
-  var baseRange = baseUnitDataRange.getRange()
-  var arr = baseRange.getValues()
+  ]*/
+  //var baseRange = baseUnitDataRange.getRange()
+  var arr = baseUnitDataRange.getValues()
   // row major order
   var airUnits = [["unit_name"].concat(relevantColumns)]
+  Logger.log("airUnits: %s", JSON.stringify(airUnits))
   columnIndices = findColumnIndices(arr, relevantColumns)
   // Iterate over each unit, creating a row for each
   for(var i=0; i < relevantUnits.length; ++i){
@@ -43,15 +50,53 @@ function calculateAirUnits(baseUnitDataRange){
     var row = [unit]
     var sourceRow = findRow(arr, unit)
     // add unit information to row array, then append to table
-    for(var j=0; j < columnIndices.length, ++j){
+    for(var j=0; j < columnIndices.length; ++j){
       row.push(sourceRow[columnIndices[j]])
     }
     airUnits.push(row)
   }
   Logger.log("creating air units sheet")
   var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = ss.insertSheet("Air")
+  // delete if it exists
+  var sheet = deleteAndCreate(ss, "Air")
   for(var i=0; i < airUnits.length; ++i){
     sheet.appendRow(airUnits[i])
   }
+}
+
+/// Helper Functions
+/** takes an array Table in row major order, and an array of column names.
+Table must have the first row as header containing column names.
+Returns an array of indices that correspond to the column names.
+*/
+function findColumnIndices(table, column_names){
+  var header = table[0]
+  var indices = []
+  for(var i=0; i < header.length; ++i){
+    if(column_names.indexOf(header[i]) > -1){
+      indices.push(i)
+    }
+  }
+  return indices
+}
+/** takes an array *table*, and a string *row_name*.
+table must be in row-major order and the first column of each row be the name.
+Returns the row which begins with row_name, or null if not found.
+*/
+function findRow(table, row_name){
+  for(var i=0; i < table.length; ++i){
+    if(table[i][0] == row_name){
+      return table[i][0]
+    }
+  }
+  return null
+}
+function deleteAndCreate(spreadsheet, sheet_name){
+  // delete if it exists
+  var sheet = spreadsheet.getSheetByName(sheet_name)
+  if(sheet){
+    spreadsheet.deleteSheet(sheet)
+  }
+  sheet = spreadsheet.insertSheet(sheet_name)
+  return sheet
 }
