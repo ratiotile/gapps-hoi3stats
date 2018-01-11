90576772d1ca375cbce782a930e366b3d269f310
ratiotile <eujain@gmail.com>
2018-01-11 13:41:57 -0500

    fixed problems with base air script

--- src/Code.js
+++ src/Code.js
@@ -27,34 +27,36 @@ function calculateAirUnits(){
   // note: air defense is bugged and doesn't work!
   var relevantColumns = ["soft_attack", "hard_attack", "air_attack",
     "strategic_attack", "range", "air_detection",
-    "default_organisation, default_morale",
+    "default_organisation", "default_morale",
     "build_cost_ic", "build_time", "supply_consumption", "fuel_consumption"
   ]
   var columnIndices = [] // corresponds to relevantColumns
-  var relevantUnits = ["interceptor"]
-
-/*
-  ["interceptor", "multi_role", "cas", "cag",
+  var relevantUnits = ["interceptor", "multi_role", "cas", "cag",
     "tactical_bomber", "naval_bomber", "strategic_bomber", "transport_plane",
     "rocket_interceptor", "flying_bomb", "flying_rocket"
-  ]*/
+  ]
   //var baseRange = baseUnitDataRange.getRange()
   var arr = baseUnitDataRange.getValues()
   // row major order
   var airUnits = [["unit_name"].concat(relevantColumns)]
-  Logger.log("airUnits: %s", JSON.stringify(airUnits))
+
   columnIndices = findColumnIndices(arr, relevantColumns)
+  Logger.log("columnIndices: %s", JSON.stringify(columnIndices))
+
   // Iterate over each unit, creating a row for each
   for(var i=0; i < relevantUnits.length; ++i){
     var unit = relevantUnits[i]
     var row = [unit]
     var sourceRow = findRow(arr, unit)
+    Logger.log("sourceRow: %s", JSON.stringify(sourceRow))
+
     // add unit information to row array, then append to table
     for(var j=0; j < columnIndices.length; ++j){
       row.push(sourceRow[columnIndices[j]])
     }
     airUnits.push(row)
   }
+  Logger.log("airUnits: %s", JSON.stringify(airUnits))
   Logger.log("creating air units sheet")
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   // delete if it exists
@@ -72,10 +74,8 @@ Returns an array of indices that correspond to the column names.
 function findColumnIndices(table, column_names){
   var header = table[0]
   var indices = []
-  for(var i=0; i < header.length; ++i){
-    if(column_names.indexOf(header[i]) > -1){
-      indices.push(i)
-    }
+  for(var i=0; i < column_names.length; ++i){
+    indices.push(table[0].indexOf(column_names[i]))
   }
   return indices
 }
@@ -86,7 +86,7 @@ Returns the row which begins with row_name, or null if not found.
 function findRow(table, row_name){
   for(var i=0; i < table.length; ++i){
     if(table[i][0] == row_name){
-      return table[i][0]
+      return table[i]
     }
   }
   return null
